4bb110fc5fa8568bf3ac76e08e98f1a28c9ae591
xuechong <xuechong87@gmail.com>
2014-08-04 09:15:21 +0800

    add invoke hava

--- Java8s/src/org/luckystars/java8tests/nashorn/InvokeJavaInjs.java
+++ Java8s/src/org/luckystars/java8tests/nashorn/InvokeJavaInjs.java
@@ -1,23 +1,23 @@
 package org.luckystars.java8tests.nashorn;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.InputStream;
+import java.net.URISyntaxException;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 
-
 public class InvokeJavaInjs {
-	
-	public static void main(String[] args) throws FileNotFoundException, ScriptException {
-		Thread.currentThread().getContextClassLoader().getResourceAsStream("InvokeJava.js");
+
+	public static void main(String[] args) throws FileNotFoundException,
+			ScriptException, URISyntaxException {
+		
+		File f = new File(Thread.currentThread().getContextClassLoader()
+				.getResource("InvokeJava.js").toURI());
+		
 		ScriptEngine engine = Utils.getNashornEngine();
-		
-		engine.eval(FileInputStream);
-		
+		engine.eval(new FileReader(f));
 		
 	}
 }
